65971aceab45ea52054bd1fdb1b28ed0cc94023c
narayansiddhu <narayansiddhu@gmail.com>
2018-04-05 14:57:15 +0530

    adding name and authors to database

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var mustacheExpress = require('mustache-express');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
+const { Client } = require('pg');
 require('dotenv').config();
-console.log(process.env)
 
-var app = express();
+// console.log(process.env)
 
-var mustache = mustacheExpress();
+const app = express();
+
+const mustache = mustacheExpress();
 mustache.cache = null;
 app.engine('mustache',mustache);
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,9 +23,27 @@ app.get('/list',(req,res)=>{
     res.render('list');
 });
 
-app.get('/bookform',(req,res)=>{
+app.get('/addbook',(req,res)=>{
     res.render('book-form');
 })
+
+app.post('/addbook',(req,res)=>{
+    console.log('post body ', req.body);
+    const client = new Client();
+    client.connect().then(()=>{
+        console.log('connection completed');
+        const sql = 'INSERT INTO books (title, authors) VALUES ($1, $2)'
+        const params =[req.body.title, req.body.authors];
+        return client.query(sql,params)
+    })
+    .then((result)=>{
+        console.log('result?',result);
+        res.redirect('/list');
+    }).catch((err) =>{
+        console.log('err',err);
+        res.redirect('/list');
+    });
+  });
 
 app.listen(process.env.PORT,()=>{
     console.log(`Listening to port ${process.env.PORT}.`);
