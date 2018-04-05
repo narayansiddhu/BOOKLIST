9833b7f715479c3624b4ea43f0193451ffdce137
narayansiddhu <narayansiddhu@gmail.com>
2018-04-05 17:18:11 +0530

    making seperate routes

--- app.js
+++ app.js
@@ -2,49 +2,23 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
-const { Client } = require('pg');
+const routes = require('./routes/index');
 require('dotenv').config();
-
 // console.log(process.env)
-
 const app = express();
-
+//template engine
 const mustache = mustacheExpress();
 mustache.cache = null;
 app.engine('mustache',mustache);
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
-
-//app.use(bodyParser.json());
+//body parser
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/list',(req,res)=>{
-    res.render('list');
-});
-
-app.get('/addbook',(req,res)=>{
-    res.render('book-form');
-})
-
-app.post('/addbook',(req,res)=>{
-    console.log('post body ', req.body);
-    const client = new Client();
-    client.connect().then(()=>{
-        console.log('connection completed');
-        const sql = 'INSERT INTO books (title, authors) VALUES ($1, $2)'
-        const params =[req.body.title, req.body.authors];
-        return client.query(sql,params)
-    })
-    .then((result)=>{
-        console.log('result?',result);
-        res.redirect('/list');
-    }).catch((err) =>{
-        console.log('err',err);
-        res.redirect('/list');
-    });
-  });
-
+//home routing
+app.use('/',routes);
+//listening
 app.listen(process.env.PORT,()=>{
     console.log(`Listening to port ${process.env.PORT}.`);
 });
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const { Client } = require('pg');
+const path = require('path');
+
+router.get('/list',(req,res)=>{
+    res.render('book-list');
+});
+router.get('/addbook',(req,res)=>{
+    res.render('book-form');
+})
+router.post('/addbook',(req,res)=>{
+    console.log('post body ', req.body);
+    const client = new Client();
+    client.connect().then(()=>{
+        console.log('connection completed');
+        const sql = 'INSERT INTO books (title, authors) VALUES ($1, $2)'
+        const params =[req.body.title, req.body.authors];
+        return client.query(sql,params)
+    })
+    .then((result)=>{
+        console.log('result?',result);
+        res.redirect('/list');
+    }).catch((err) =>{
+        console.log('err',err);
+        res.redirect('/book-list');
+    });
+  });
+module.exports = router;
